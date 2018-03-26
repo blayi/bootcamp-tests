6c8e31dde85ea4ba9c86d07aad72fe082a2c54fe
blayi <siyasangablayi18@gmail.com>
2018-03-26 12:09:32 +0200

    made new changes

--- test/findItemsOver.test.js
+++ test/findItemsOver.test.js
@@ -1,11 +1,11 @@
 describe('findItemsOver' , function(){
-    it('confirms that plates  are from Paarl' , function(){
+    it('confirms that items are over' , function(){
 assert.deepEqual(findItemsOver([{name : 'apples', qty : 10},
     {name : 'pears', qty : 37}],20),[ {"name" : 'pears', "qty" : 37}])
 
   });
 
-    it('confirm that plates are not from Paarl ' , function(){
+    it('confirm that items are over' , function(){
 assert.deepEqual(findItemsOver([ {name : 'bananas', qty : 27},
     {name : 'apples', qty : 3}],20), [ {"name" : 'bananas', "qty" : 27}]);
   });
